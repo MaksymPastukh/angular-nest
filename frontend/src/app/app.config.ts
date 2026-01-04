2c96904af4ff5fe41d546de8c4870c4c68bd66ee
Maksym Pastukh <118793677+MaksymPastukh@users.noreply.github.com>
2026-01-04 13:26:59 +0100

    feat(frontend): enhance router configuration with view transitions and in-memory scrolling

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,18 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
-import { provideRouter } from '@angular/router'
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core'
+import {provideRouter, withInMemoryScrolling, withViewTransitions} from '@angular/router'
 
-import { routes } from './app.routes'
-import { provideHttpClient, withInterceptors } from '@angular/common/http'
-import { AuthInterceptor } from './core/auth/services/auth.interceptor'
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
-import { providePrimeNG } from 'primeng/config'
+import {routes} from './app.routes'
+import {provideHttpClient, withInterceptors} from '@angular/common/http'
+import {AuthInterceptor} from './core/auth/services/auth.interceptor'
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async'
+import {providePrimeNG} from 'primeng/config'
 import Aura from '@primeuix/themes/aura'
-import { MessageService } from 'primeng/api'
+import {MessageService} from 'primeng/api'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes,
+      withViewTransitions(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top', // или 'enabled'
+      })),
     provideHttpClient(withInterceptors([AuthInterceptor])),
     provideAnimationsAsync(),
     providePrimeNG({
